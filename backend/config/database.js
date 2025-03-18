d9ac450c6c2775049d9210ee298c2796a1e96afd
Jolivan Abreu <jolivan489@gmail.com>
2025-03-18 01:59:21 -0300

    Update

--- backend/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("venda_frutas", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-module.exports = sequelize;
